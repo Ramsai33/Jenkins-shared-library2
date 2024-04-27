3718a679e7c1ddb41cee187af042d2074e0789a6
mudur <mudurukollaramsai335@gmail.com>
2024-04-27 20:40:11 +0530

    scripted pipeline

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -5,7 +5,19 @@ def call() {
             git branch: 'main', url: "https://github.com/Ramsai33/${component}.git"
             sh 'env'
         }
-
+        stage('Build') {
+            common.compile()
+        }
+        stage('UnitTest') {
+            common.unittest()
+        }
+        stage('QualityCheck') {
+            SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+            SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                sh "sonar-scanner -Dsonar.host.url=http://172.31.31.13:9000 -Dsonar.login=$SONAR_USER -Dsonar.password=$SONAR_PASS -Dsonar.projectKey=${component} ${SONAR_EXTRA_PASS}"
+            }
+        }
     }
 
 
