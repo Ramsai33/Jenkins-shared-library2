b63c3a66c41b8f5dfb66b396fd9ff7054e4bed26
mudur <mudurukollaramsai335@gmail.com>
2024-04-27 20:08:04 +0530

    Single banch pipeline

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -25,7 +25,8 @@ def call() {
                 steps {
                     environment {
                     SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                    SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                    SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        //, returnStdout: true).trim()
                     }
                         script {
                         sh "sonar-scanner -Dsonar.host.url=http://172.31.31.13:9000 -Dsonar.login=$SONAR_USER -Dsonar.password=$SONAR_PASS -Dsonar.projectKey=${component}"
